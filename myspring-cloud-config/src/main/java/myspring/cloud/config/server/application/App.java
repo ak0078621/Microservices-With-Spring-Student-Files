8f1846a979790f0eb511dd25ce936e06e00c5bfd
ak0078621 <ak0078621@gmail.com>
2019-08-03 14:01:54 -0400

    new changes
    Micoservices

---
+++ myspring-cloud-config/src/main/java/myspring/cloud/config/server/application/App.java
@@ -0,0 +1,16 @@
+package myspring.cloud.config.server.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@SpringBootApplication
+@EnableConfigServer
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class,args);
+    }
+}
